9e6ab8ce22d5db1606eec661f5f92c88aef50a22
ericrius1 <ericrius1@gmail.com>
2015-12-29 09:04:59 -0800

    photos

--- js/painting.js
+++ js/painting.js
@@ -3,7 +3,7 @@ var Painting = function() {
 
     this.geometry = new THREE.PlaneGeometry(540, 960);
     this.material = new THREE.MeshBasicMaterial({
-        map: textureLoader.load("assets/rel.png"),
+        map: textureLoader.load("assets/rel3.png"),
         transparent: true,
         opacity: 1,
         // side: THREE.DoubleSide
@@ -12,5 +12,21 @@ var Painting = function() {
     this.mesh = new THREE.Mesh(this.geometry, this.material);
 
     scene.add(this.mesh);
+
+    var geometry = new THREE.PlaneGeometry(960, 720);
+    var material = new THREE.MeshBasicMaterial({
+        map: textureLoader.load("assets/sunset2.png"),
+        transparent: true,
+        opacity: 1,
+        // side: THREE.DoubleSide
+        alphaTest: 0.5
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z -= 1000;
+    mesh.scale.multiplyScalar(3.5);
+
+    scene.add(mesh);
+
+
 
 }
